0f25e8d3a4a892aa62f97c2be33824d9261edbc0
Ricardas <ricardas.serneckas@ktu.edu>
2021-10-04 15:20:55 +0300

    fixes

--- api/index.js
+++ api/index.js
@@ -24,5 +24,5 @@ app.use("/api/tasks", taskRoute);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("Backend running");
+  console.log("Backend running on port " + port);
 });
